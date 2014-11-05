8c4f330e496330d3e2b3aa72cb6e1af076711a0d
David Sansome <me@davidsansome.com>
2014-11-05 21:03:21 +1100

    Put /target on the persistent data volume and initialise it when the mingw slave is first run

--- docker/config/slave/start.py
+++ docker/config/slave/start.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import subprocess
 
 slavename = open('/slave-name').read().strip()
 basedir = os.path.join('/persistent-data', slavename)
@@ -8,6 +10,18 @@ if not os.path.exists(basedir):
   os.mkdir(basedir)
   os.symlink('/config/slave/buildbot.tac', os.path.join(basedir, 'buildbot.tac'))
   os.symlink('/config/slave/info', os.path.join(basedir, 'info'))
+
+  if os.path.exists('/first-time-setup.sh'):
+    try:
+      stdout = subprocess.check_output(
+          ['/bin/sh', '/first-time-setup.sh'],
+          stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+      shutil.rmtree(basedir)
+      raise
+
+    with open(os.path.join(basedir, 'first-time-setup.log'), 'w') as fh:
+      fh.write(stdout)
 
 os.execl(
     '/usr/local/bin/buildslave',
---
+++ docker/slave-mingw/first-time-setup.sh
@@ -0,0 +1,7 @@
+#/bin/sh -e
+
+mkdir -v -p /persistent-data/mingw/target/bin \
+            /persistent-data/mingw/target/stow \
+            /persistent-data/mingw/windows-dependencies/source
+
+ln -v -s /mingw/i686-w64-mingw32/lib/libgcc_s_sjlj-1.dll /target/bin/
